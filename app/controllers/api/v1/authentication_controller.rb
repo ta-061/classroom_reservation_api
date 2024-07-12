464a47de590462d8ace9e0ea525d04a8c9d7399b
ta-061 <fjttrj67xm@i.softbank.jp>
2024-07-13 03:39:45 +0900

    デバックよう

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::AuthenticationController < ApplicationController
   SECRET_PASSWORD = ENV['SECRET_PASSWORD'] # 環境変数から合言葉を読み込む
 
   def authenticate
+    Rails.logger.debug "SECRET_PASSWORD: #{SECRET_PASSWORD}"
+    Rails.logger.debug "Params password: #{params[:password]}"
+    
     if params[:password] == SECRET_PASSWORD
       render json: { message: 'Authenticated' }, status: :ok
     else
