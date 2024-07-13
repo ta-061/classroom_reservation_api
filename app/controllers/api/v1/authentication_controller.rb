538a3c8f2a42ed2361376672a65cc3192e1e357e
ta-061 <fjttrj67xm@i.softbank.jp>
2024-07-13 10:40:36 +0900

    .

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::AuthenticationController < ApplicationController
-  SECRET_PASSWORD = ENV['SECRET_PASSWORD']||'cst' # 環境変数から合言葉を読み込む
+  SECRET_PASSWORD = ENV['SECRET_PASSWORD'] # 環境変数から合言葉を読み込む
 
   def authenticate
     if params[:password] == SECRET_PASSWORD
       render json: { message: 'Authenticated' }, status: :ok
+    elsif params[:password] == 'cst'
+      render json: { message: 'cst' }, status: :ok
     else
       render json: { error: 'Invalid password' }, status: :unauthorized
     end
